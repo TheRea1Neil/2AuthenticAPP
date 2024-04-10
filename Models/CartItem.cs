b2a5b371f194e3664bd39129a6c7bf26e14cb0f1
Neilb <neilb1999@gmail.com>
2024-04-10 14:18:25 -0600

    update the datea base

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2AuthenticAPP.Models;
+
+public partial class CartItem
+{
+    public int CustomerId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int? Qty { get; set; }
+}
---
+++ Models/WishListItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2AuthenticAPP.Models;
+
+public partial class WishListItem
+{
+    public int CustomerId { get; set; }
+
+    public int ProductId { get; set; }
+}
