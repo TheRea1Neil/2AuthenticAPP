5f636575c799fe88bee014691c89e91968609206
jfails1 <jfails@byu.edu>
2024-04-09 22:44:08 -0600

    log in page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,15 @@ namespace _2AuthenticAPP.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
