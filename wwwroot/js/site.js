778cf618e05c15811df6e0573cc914f6ee15b473
jfails1 <jfails@byu.edu>
2024-04-11 21:49:40 -0600

    blah blah

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,16 +1,17 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-document.addEventListener('DOMContentLoaded', (event) => {
-    var acceptButton = document.getElementById('acceptCookies'); // Make sure this is the correct ID for your accept button
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    var acceptButton = document.getElementById('acceptCookies');
 
     acceptButton.addEventListener('click', function () {
-        // Set the cookie to record the user's acceptance
-        document.cookie = "cookieConsent=true; max-age=86400; path=/"; // 86400 seconds = 1 day
-
-        // Hide the cookie consent element
+        // Existing code to set the cookie and hide the consent element.
+        document.cookie = "cookieConsent=true; max-age=86400; path=/;";
         document.getElementById('cookieConsentContainer').style.display = 'none';
     });
-});
 
+    // Add your search functionality here
+    var searchInput = document.querySelector('.search-input');
+    if (searchInput) {
+        searchInput.addEventListener('keyup', function () {
+            searchProducts();
+        });
+    }
+});
