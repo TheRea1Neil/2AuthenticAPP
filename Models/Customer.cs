3f7aaa6978bc38c3dc4d85554e3a5aa4ac14de88
Neilb <neilb1999@gmail.com>
2024-04-12 00:43:00 -0600

    MML scafold

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2AuthenticAPP.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public string? CountryOfResidence { get; set; }
+
+    public string? Gender { get; set; }
+
+    public double? Age { get; set; }
+
+    public string? Email { get; set; }
+}
